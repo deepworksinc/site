6e9ab973dc01d01693c6279daa5e284ab762a17d
shanejearley <shanejearley@users.noreply.github.com>
2021-11-10 19:40:12 +0000

    deploy: 2f3b8dcf8c83b23799e5a3cc9623d807e022ad49

---
+++ server/client.manifest.mjs
@@ -0,0 +1,10 @@
+export default {
+  "node_modules/nuxt3/dist/app/entry.mjs": {
+    "file": "entry-f9ff55ea.mjs",
+    "src": "node_modules/nuxt3/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "assets/entry.7b732a41.css"
+    ]
+  }
+}
